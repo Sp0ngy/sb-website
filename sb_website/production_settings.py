8196ba91d44661aa66234f6d477790ae33da5d43
Nico.Schuerrle <nicohome0809@web.de>
2022-05-18 14:52:24 +0200

    - extended production_settings.py - added STATIC_ROOT - perpare for first push on heroku

--- sb_website/production_settings.py
+++ sb_website/production_settings.py
@@ -156,7 +156,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = ''
+STATIC_ROOT = 'staticfiles/'
 
 # Static files uploaded by a user during development
 
