d7110e507929f33a2a0ebfa0b32705f84f6331e4
Nico.Schuerrle <nicohome0809@web.de>
2022-05-19 15:28:40 +0200

    Added relative path to STATIC_ROOT

--- sb_website/dev_settings.py
+++ sb_website/dev_settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Static files uploaded by a user during development
 
